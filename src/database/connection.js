2143acb3451b0f8a012a9c64359de0bcd71e46df
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-05-08 17:48:30 -0300

    Still trying to make render work

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,25 +1,14 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-export const connection = new Sequelize(
-  // process.env.DB_BASE, // DataBase
-  // process.env.DB_USER, // User
-  // process.env.DB_PASS, // Password
-  process.env.DB_URL,
-  // {
-  //   host: process.env.DB_LOCAL, //URL from the databank
-  //   port: 5432, //The database port
-  //   dialect: "postgres", //What database is getting worked on
-  // },
-  {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+export const connection = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 module.exports = connection;
